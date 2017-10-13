159ef66e942cd6121e2faa0db84d660ae35a37ca
mars <627556038@qq.com>
2017-10-13 17:16:04 +0800

    删除多余方法

--- HXProgress/HXProgress.h
+++ HXProgress/HXProgress.h
@@ -30,8 +30,6 @@ static NSString * msg = @"加载中..."; //hud msg
  Toast提示框
  */
 + (void)setToastPosition:(id)position style:(CSToastStyle *)style isTapDismiss:(BOOL)isTapDismiss;
-
-+ (void)setToastStyle:(void(^)(void))style;
 + (void)showToastWithMsg:(NSString *)msg;
 + (void)showToastWithMsg:(NSString *)msg showTime:(CGFloat)showTime;
 
